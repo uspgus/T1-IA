50ea7eaf66669d8fc24ba3e597d65c247f9b1b1d
Gustavo Ferreira <gusferreira1203@usp.br>
2024-09-27 00:26:31 -0300

    initial files created

---
+++ graph.pl
@@ -0,0 +1,1 @@
+% File for graph search logic
---
+++ main.py
@@ -0,0 +1,6 @@
+# Master file
+# Manages input, storage and presentation, only calling upon graph.pl to handle graph search logic
+
+# Guilherme Abreu, Gustavo Ferreira, Murilo Zabott, Lucas Omati
+# 2nd Semester 2024
+# University of São Paulo, Institute of Mathematical and Computer Science
