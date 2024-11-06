e2c4f89b42f64269fb01d9700e8fd7278a071772
Joaofarj <87398892+Joaofarj@users.noreply.github.com>
2024-11-06 15:26:13 -0300

    Add files via upload
    Gera_Rede_Geográfica gera redes do jeito que o professor pediu. "largura" e "profundidade" são as implementações das buscas de largura e profundidade usando redes reais (osmnx). Nós colocamos para plotar o melhor caminho e o pior caminho que a busca achou.

---
+++ Gera_Rede_Geográfica.py
@@ -0,0 +1,30 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def dist(grafo, a, b):
+    return np.linalg.norm(grafo.nodes[a]['pos'] - grafo.nodes[b]['pos'])
+
+def gera_grafo(n, l):
+    grafo = nx.Graph()
+    #np.random.seed(0)
+
+    #Gera Nós
+    vertices = np.random.rand(n, 2)*n
+    grafo.add_nodes_from([ (i, dict(pos=vert)) for i, vert in enumerate(vertices) ])
+
+    #Gera Arestas
+    for i in range(len(vertices)):
+        for j in range(i):
+            if (dist(grafo, i, j)*l < np.random.rand()):
+                grafo.add_edge(i, j)
+
+    return grafo
+
+grafo = gera_grafo(500, 0.011)
+
+
+##Plota o grafo
+nx.draw(grafo, nx.get_node_attributes(grafo, 'pos'), node_size=10)
+plt.show()
