d5ee5b62654ec8fc900a0c1ab330f5256ab6e6d4
Murilo Valentim Zabott <murilo.zabott@usp.br>
2024-11-06 22:46:01 -0300

    busca best-first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # Master file
-# Manages input, storage and presentation, only calling upon graph.pl to handle graph search logic
 
-# Guilherme Abreu, Gustavo Ferreira, Murilo Zabott, Lucas Omati
+# Gustavo Ferreira, Murilo Zabott, João Pedro Farjoun, Tomás Ribeiro
 # 2nd Semester 2024
 # University of São Paulo, Institute of Mathematical and Computer Science
